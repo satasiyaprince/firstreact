6d95ab7868e19d0ebdf186bea0f72cd94615d683
satasiyaprince <princesatasiya49@gmail.com>
2022-07-13 11:46:15 +0530

    13_7_22_learn

--- src/App.js
+++ src/App.js
@@ -3,69 +3,66 @@ import './App.css';
 
 function App() {
 
-let Data = [
+  let Data =
+[
     {
-      id: 101,
-      name: 'Abacavir',
-      quantity: 25,
-      price: 150,
-      expiry: 2022,
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
       status: true
     },
     {
-      id: 102,
-      name: 'Eltrombopag',
-      quantity: 90,
-      price: 550,
-      expiry: 2021,
-      status: true
-    },
-    {
-      id: 103,
-      name: 'Meloxicam',
-      quantity: 85,
-      price: 450,
-      expiry: 2025,
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
       status: false
     },
     {
-      id: 104,
-      name: 'Allopurinol',
-      quantity: 50,
-      price: 600,
-      expiry: 2023,
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
       status: true
     },
     {
-      id: 105,
-      name: 'Phenytoin',
-      quantity: 63,
-      price: 250,
-      expiry: 2021,
-      status: false
-    }
-  ];
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
+  ]
 
-    
-  let fdata = Data.filter((v,i) => v.expiry >= 2022 && v.status === true);
-  console.log(fdata);
+     let fdata =  Data.filter((v,i) => v.status === true);
+     console.log(fdata);
 
-  let Totalprice = fdata.reduce((acc,v,i) => acc + v.price ,0);
-  console.log(Totalprice);
+    let Totalsalary = fdata.reduce((acc,v,i) => acc + v.salary + v.bonus ,0);
+    console.log(Totalsalary);
 
   return (
-
-  <div>{
-    fdata.map((v,i) => {
-      return(
-        <div key={i}>
-          <h1>{v.name}</h1>
-          <h2>{v.price}</h2>
-          <h3>{v.id}</h3>
-        </div>
-        )
-    })
-    }</div>
+    <div>
+      {
+        fdata.map((v,i) => {
+          return(
+            <div key={i}>
+               <h1>{v.name}</h1>
+               <h2>{v.salary}</h2>
+               <h3>{v.bonus}</h3>
+               <h4>{v.salary + v.bonus}</h4>
+            </div>
+          )
+        })
+      }
+    </div>
   );
 }
 

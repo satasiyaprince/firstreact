097d085e226260d4dc2c5d07fd14ad2872fa74da
satasiyaprince <princesatasiya49@gmail.com>
2022-07-14 10:24:54 +0530

    14_7_22_Student Data

--- src/App.js
+++ src/App.js
@@ -2,71 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let Data =
+  [
+      {
+        grid: 123,
+        name: 'Amit',
+        fees: 50000,
+        attendance: 80
+      },
+      {
+        grid: 456,
+        name: 'Ajay',
+        fees: 70000,
+        attendance: 90
+      },
+      {
+        grid: 856,
+        name: 'Ashish',
+        fees: 30000,
+        attendance: 65
+      },
+      {
+        grid: 762,
+        name: 'Raj',
+        fees: 90000,
+        attendance: 70
+      },
+    ]
 
-let Data = [
-    {
-      id: 101,
-      name: 'Abacavir',
-      quantity: 25,
-      price: 150,
-      expiry: 2022,
-      status: true
-    },
-    {
-      id: 102,
-      name: 'Eltrombopag',
-      quantity: 90,
-      price: 550,
-      expiry: 2021,
-      status: true
-    },
-    {
-      id: 103,
-      name: 'Meloxicam',
-      quantity: 85,
-      price: 450,
-      expiry: 2025,
-      status: false
-    },
-    {
-      id: 104,
-      name: 'Allopurinol',
-      quantity: 50,
-      price: 600,
-      expiry: 2023,
-      status: true
-    },
-    {
-      id: 105,
-      name: 'Phenytoin',
-      quantity: 63,
-      price: 250,
-      expiry: 2021,
-      status: false
-    }
-  ];
+    let fdata = Data.filter((v,i) => v.attendance < 71);
+    console.log(fdata);
 
-    
-  let fdata = Data.filter((v,i) => v.expiry >= 2022 && v.status === true);
-  console.log(fdata);
-
-  let Totalprice = fdata.reduce((acc,v,i) => acc + v.price ,0);
-  console.log(Totalprice);
+    let total = fdata.reduce((acc,v,i) => acc + v.fees ,0)
+    console.log(total);
 
   return (
-
-  <div>{
-    fdata.map((v,i) => {
-      return(
-        <div key={i}>
-          <h1>{v.name}</h1>
-          <h2>{v.price}</h2>
-          <h3>{v.id}</h3>
-        </div>
-        )
-    })
-    }</div>
-  );
+    <div>
+      {
+          fdata.map((v,i) => {
+            return(
+              <div key={i}>
+                     <h3>{v.name}</h3>
+                     <h4>{v.grid}</h4>
+                     <h5>{v.fees}</h5>
+              </div>
+            )
+          })
+      }
+    </div>
+  );    
 }
 
 export default App;

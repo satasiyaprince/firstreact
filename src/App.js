80a815c0d33f9f43962779391a35e63af47cb52e
satasiyaprince <princesatasiya49@gmail.com>
2022-07-14 10:43:59 +0530

    13_7_22_Employee-data

--- src/App.js
+++ src/App.js
@@ -4,63 +4,67 @@ import './App.css';
 function App() {
 
   let Data =
-[
-    {
-      name: "amit",
-      age: 35,
-      salary: 40000,
-      bonus: 1000,
-      status: true
-    },
-    {
-      name: "ajay",
-      age: 25,
-      salary: 38000,
-      bonus: 2000,
-      status: false
-    },
-    {
-      name: "mayur",
-      age: 23,
-      salary: 50000,
-      bonus: 500,
-      status: true
-    },
-    {
-      name: "jay",
-      age: 29,
-      salary: 35000,
-      bonus: 600,
-      status: true
-    },
-    {
-      name: "raj",
-      age: 33,
-      salary: 22000,
-      bonus: 2000,
-      status: true
-    },
-  ]
+    [
+      {
+        name: "amit",
+        age: 35,
+        salary: 40000,
+        bonus: 1000,
+        status: true
+      },
+      {
+        name: "ajay",
+        age: 25,
+        salary: 38000,
+        bonus: 2000,
+        status: false
+      },
+      {
+        name: "mayur",
+        age: 23,
+        salary: 50000,
+        bonus: 500,
+        status: true
+      },
+      {
+        name: "jay",
+        age: 29,
+        salary: 35000,
+        bonus: 600,
+        status: true
+      },
+      {
+        name: "raj",
+        age: 33,
+        salary: 22000,
+        bonus: 2000,
+        status: true
+      },
+    ]
 
-     let fdata =  Data.filter((v,i) => v.status === true);
-     console.log(fdata);
+  let fdata = Data.filter((v, i) => v.status === true);
+  console.log(fdata);
 
-    let Totalsalary = fdata.reduce((acc,v,i) => acc + v.salary + v.bonus ,0);
-    console.log(Totalsalary);
+  let Totalsalary = fdata.reduce((acc, v, i) => acc + v.salary + v.bonus, 0);
+  console.log(Totalsalary);
 
   return (
-  <div>{
-    fdata.map((v,i) => {
-      return(
-        <div key={i}>
-          <h1>{v.name}</h1>
-          <h2>{v.price}</h2>
-          <h3>{v.id}</h3>
-          <h4>{v.status.toString}</h4>
-        </div>
-        )
-    })
-    }</div>
+    <div>
+      {
+        fdata.map((v, i) => {
+          return (
+            <div key={i}>
+              <h1>{v.name}</h1>
+              <h2>{v.salary}</h2>
+              <h3>{v.bonus}</h3>
+              <h5>{v.salary + v.bonus}</h5>
+              <h4>{v.status.toString()}</h4>
+            </div>
+          )
+        })
+      }
+      <h3>Total :- {Totalsalary}</h3>
+    </div>
   );
 }
 

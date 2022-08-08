66728c0843cc62c7821b8fd61c714faec80240ae
satasiyaprince <princesatasiya49@gmail.com>
2022-08-08 09:57:51 +0530

    08_8_22_component

--- src/Function/Time.js
+++ src/Function/Time.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class Time extends Component {
+    //1.Provides Initial Value
     constructor(props) {
         super(props);
 
@@ -15,10 +16,24 @@ class Time extends Component {
         })
     }
 
+    //3. sent reqvest to a servar for data.
     componentDidMount = () => {
-        setInterval(() => this.tick(),1000)
+        this.time = setInterval(() => this.tick(),1000)
     }
 
+    //4. Whenevar Previonvs state or Props is Updatad then it is calld .
+    componentDidUpdate = (prevProps, prevState) => {
+        if(prevState.time !== this.state.time){
+            console.log("componentDidUpdate");
+        }
+    }
+
+    //5. Whenevar we move to another component then it is called. it resources. 
+    componentWillUnmount =() => {
+        clearInterval(this.time);
+    }
+
+    //2. Disply on the scren .whenever sate value changed render method is called
     render() {
         return (
             <div>

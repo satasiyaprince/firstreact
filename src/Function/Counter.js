16cd0add923bbc46fe42728acf44e399df4b5080
satasiyaprince <princesatasiya49@gmail.com>
2022-08-30 10:25:41 +0530

    30_8_Counter

--- src/Function/Counter.js
+++ src/Function/Counter.js
@@ -2,19 +2,23 @@ import React, { useState } from 'react';
 
 function Counter(props) {
 
-const [count,setCount] = useState (0); 
+    const [count, setCount] = useState(0);
 
-    const Counteragreement =() =>{
-          setCount(count+1);  
+    const Counteragreement = () => {
+        if (count < 5) {
+            setCount(count + 1);
+        }
     }
 
-    const Coounterdecrement =() =>{
-            setCount(count-1);
+    const Coounterdecrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
     }
     return (
 
-       
-        
+
+
         <div>
             <button onClick={() => Counteragreement()}>+</button>
             <span>{count}</span>
